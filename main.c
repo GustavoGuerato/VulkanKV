b78ea42f61820df3f0c95ae13ca564a0e746310e
GueratoGustavo <guerato.gustavo@gmail.com>
2026-05-29 15:38:57 -0300

    bind and listen creation

--- main.c
+++ main.c
@@ -13,4 +13,22 @@ int main(void)
     printf("Error to create socket\n");
     exit(EXIT_FAILURE);
   }
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(8080);
+
+  if (bind(server_sd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+  {
+    printf("Error to bind socket\n");
+    exit(EXIT_FAILURE);
+  }
+  if (listen(server_sd, 3) < 0)
+  {
+    printf("Error to listen socket\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("Server is listening on port 8080\n");
+  close(server_sd);
+  return 0;
 }
